fdaa1ce00ea6954e9924cd622270cefe280ffa24
riccardo <riccagallo67@gmail.com>
2021-03-29 15:32:15 +0200

    esBagno

---
+++ esBagno/src/esbagno/runnable1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esbagno;
+
+/**
+ *
+ * @author PC
+ */
+public class runnable1 implements Runnable {
+    
+    public void run(){
+        bagno b1= new bagno();
+        b1.bagnoUomo();
+    }
+    
+    
+    
+}
---
+++ esBagno/src/esbagno/runnable2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esbagno;
+
+/**
+ *
+ * @author PC
+ */
+public class runnable2 implements Runnable {
+    public void run(){
+        bagno2 b2= new bagno2();
+        b2.bagnoDonna();
+    }
+    
+    
+}
